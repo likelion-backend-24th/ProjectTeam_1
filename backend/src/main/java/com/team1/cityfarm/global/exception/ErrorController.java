7e2a0cf55b4dbcaaf35b6ba6e08a1b722809731d
songsihun <ssh970520@gmail.com>
2026-08-04 14:06:55 +0900

    global exception 작성중

---
+++ backend/src/main/java/com/team1/cityfarm/global/exception/ErrorController.java
@@ -0,0 +1,13 @@
+//package com.team1.cityfarm.global.exception;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class ErrorController {
+//
+//    @GetMapping("/errorcheck")
+//    public String errorCheck(){
+//        throw new CustomException(CustomError.AUTH_DUPLICATED_EMAIL);
+//    }
+//}
