342efd8437a286d1204797208fd680ae2f5ae4f0
songsihun <ssh970520@gmail.com>
2026-08-06 16:11:28 +0900

    GlobalExceptionHandler가 apiResponse 응답하도록 수정

--- backend/src/main/java/com/team1/cityfarm/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/team1/cityfarm/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.team1.cityfarm.global.exception;
 
+import com.team1.cityfarm.global.response.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,11 +11,19 @@ public class GlobalExceptionHandler {
 
     //CustomError Enum으로 정의한 예외 처리
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<CustomResponse> handleCustomException(CustomException e) {
+    public ResponseEntity<ApiResponse<?>> handleCustomException(CustomException e) {
         CustomError customError = e.getCustomError();
 
         return ResponseEntity
                 .status(customError.getHttpStatus())
-                .body(CustomResponse.of(customError));
+                .body(ApiResponse.error(customError.name(), customError.getMessage()));
+    }
+
+    // 알 수 없는 서버 내부 에러(500) 일괄 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다."));
     }
 }
