086fc3c50b4159dfb4f66f294d4d105d6150066c
jake1031 <jake1031@naver.com>
2026-08-07 13:21:36 +0900

    fix: Swagger api 문서용 애노테이션 추가

--- backend/src/main/java/com/team1/cityfarm/controller/BoardCommentController.java
+++ backend/src/main/java/com/team1/cityfarm/controller/BoardCommentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "Board Comment", description = "게시글 댓글 관련 API")
+@Tag(name = "게시글 댓글 API", description = "게시글 댓글 조회, 등록, 수정, 삭제 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
