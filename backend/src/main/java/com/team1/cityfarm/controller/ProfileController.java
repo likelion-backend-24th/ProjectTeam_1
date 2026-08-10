35bdd5c7ee7f2048b5836ea484a42312e1fb0309
jake1031 <jake1031@naver.com>
2026-08-10 11:23:55 +0900

    feat: DB 연결 테스트5

--- backend/src/main/java/com/team1/cityfarm/controller/ProfileController.java
+++ backend/src/main/java/com/team1/cityfarm/controller/ProfileController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "프로필 API", description = "마이 프로필 조회 API")
+@Tag(name = "프로필 API", description = "내 프로필 조회 API")
 @SecurityRequirement(name = "BearerAuth")
 @RestController
 @RequiredArgsConstructor
