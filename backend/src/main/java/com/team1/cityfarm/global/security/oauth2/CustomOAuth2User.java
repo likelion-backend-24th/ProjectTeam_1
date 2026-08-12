47c8d79a1e1359ef4776fe9a5164d726c4c33337
jake1031 <jake1031@naver.com>
2026-08-13 00:06:25 +0900

    feat: 구글 OAuth 코드 수정

--- backend/src/main/java/com/team1/cityfarm/global/security/oauth2/CustomOAuth2User.java
+++ backend/src/main/java/com/team1/cityfarm/global/security/oauth2/CustomOAuth2User.java
@@ -13,11 +13,13 @@ import java.util.Map;
 @Getter
 public class CustomOAuth2User implements OAuth2User {
 
-    private final User user;
+    private final String email;
+    private final String role;
     private final Map<String, Object> attributes;
 
     public CustomOAuth2User(User user, Map<String, Object> attributes) {
-        this.user = user;
+        this.email = user.getEmail();
+        this.role = user.getRoleType().name();
         this.attributes = attributes;
     }
 
@@ -28,11 +30,11 @@ public class CustomOAuth2User implements OAuth2User {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority(user.getRoleType().name()));
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
     }
 
     @Override
     public String getName() {
-        return user.getEmail();
+        return email;
     }
 }
