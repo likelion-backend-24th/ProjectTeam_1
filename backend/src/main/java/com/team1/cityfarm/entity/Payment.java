03076881f70ba5a9dba2a461f277aae44abf76cb
송시훈 <ssh970520@gmail.com>
2026-08-18 14:32:30 +0900

    order,subscription 추가

---
+++ backend/src/main/java/com/team1/cityfarm/entity/Payment.java
@@ -0,0 +1,20 @@
+package com.team1.cityfarm.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Payment {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ backend/src/main/java/com/team1/cityfarm/entity/SubscriptionPassUsage.java
@@ -0,0 +1,21 @@
+package com.team1.cityfarm.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class SubscriptionPassUsage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
